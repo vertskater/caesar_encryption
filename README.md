d2c3c102a48bf15fce1e2db228ea9e9bc16ded60
vertskater <vertskater@gmx.at>
2020-06-16 19:29:30 +0200

    Wrote Function in caesar.js file, UI with HTML and CSS -- Ver 1.0

---
+++ README.md
@@ -0,0 +1,9 @@
+This is a small JavaScript project for Text "encryption".
+The Caesar encryption shifts a string to a given position in the alphabeth.
+
+Function input = string, number
+Function output = shifts the letter a given number
+e.g function(A, 2) returns C;
+
+This Function also works backwards: e.g function(A, -2) returns Y 
+
---
+++ caesar.js
@@ -0,0 +1,56 @@
+'use strict'
+let btn = document.querySelector('.btn-cypher');
+
+const caesar = function (text, nr) {
+    (nr % 26 == 0 ? nr = 26 : nr = nr % 26);
+    return text
+        .split("")
+        .map(letter => shiftChar(letter, nr))
+        .join("");
+};
+
+function shiftChar(letter, nr) {
+    let newNr = letter.charCodeAt();
+    if (newNr == 33 || newNr == 44 || newNr == 32) {
+        return letter;
+    }
+    if (nr > 0) {
+        if (newNr > 96) {
+            newNr += nr;
+            if (newNr > 122) {
+                newNr -= 26
+            }
+            return letter = String.fromCharCode(newNr);
+        } else if (newNr > 64) {
+            newNr += nr;
+            if (newNr > 90) {
+                newNr -= 26;
+            }
+            return letter = String.fromCharCode(newNr);
+        }
+    }else{
+        if(newNr > 96){
+            newNr += nr;
+            if(newNr < 97){
+                newNr = 122 - (96 - newNr);
+            }
+            return letter = String.fromCharCode(newNr);
+       } else if((newNr > 64)/* && (newNr < 90)*/){
+            newNr += nr;
+            if(newNr < 65){
+                newNr = 90 - (64 - newNr);
+            }
+            return letter = String.fromCharCode(newNr);
+        }
+    }
+}
+
+
+
+btn.addEventListener('click', ()=>{
+    let solution = document.querySelector('.solution');
+    let text = document.getElementById('inputtext').value;
+    let nr = document.getElementById('number').value;
+    let cypherText = caesar(text, nr);
+    solution.textContent = cypherText;
+})
